4f72a5365f743b0a273551e57dfea30d8bcb4a0c
siho-song <thdtlgh234@naver.com>
2023-03-27 20:57:43 +0900

    modified

--- scripts/bokgpt_deploy.sh
+++ scripts/bokgpt_deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
 PROJECT_NAME="bokgpt"
-PROJECT_PATH="/home/ubuntu/$PROJECT_NAME"
 JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
-DEPLOY_PATH="$PROJECT_PATH/deploy"
-DEPLOY_LOG_PATH="$PROJECT_PATH/log/deploy.log"
-DEPLOY_ERR_LOG_PATH="$PROJECT_PATH/log/deploy_err.log"
-APPLICATION_LOG_PATH="$PROJECT_PATH/log/application.log"
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "====== 배포 시작 : $(date +%c) ======" >> $DEPLOY_LOG_PATH
-echo "> Build file name: $JAR_NAME" >> $DEPLOY_LOG_PATH
-echo "> Build file copy" >> $DEPLOY_LOG_PATH
+echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
+
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> Current running pid check..." >> $DEPLOY_LOG_PATH
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
