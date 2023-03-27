771191978ec799d99af1ec71f2eff86472e13f16
siho-song <thdtlgh234@naver.com>
2023-03-27 15:48:40 +0900

    first

---
+++ bokgpt-server/src/main/java/com/example/bokgptserver/BokgptServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bokgptserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokgptServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokgptServerApplication.class, args);
+    }
+
+}
