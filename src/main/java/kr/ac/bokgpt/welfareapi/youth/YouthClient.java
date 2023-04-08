5fe2e04c165387d44dd6942568ef89087d95cd1d
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-04-08 17:25:55 +0900

    refactor: 복지 정보 가져오기 api 주석 처리
    복지정보 key를 바꾸는 과정에서 오류발생
    임시로 주석 처리

--- src/main/java/kr/ac/bokgpt/welfareapi/youth/YouthClient.java
+++ src/main/java/kr/ac/bokgpt/welfareapi/youth/YouthClient.java
@@ -1,19 +1,18 @@
 package kr.ac.bokgpt.welfareapi.youth;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class YouthClient {
 
-    @Value("${centerwelfare.client.id}")
-    private static String YouthClientId;
-
-    @Value("${centerwelfare.client.secret}")
-    private static String YouthSecret;
-
-    @Value("${centerwelfare.url.}")
-    private static String YouthWelfareUrl;
+//    @Value("${centerwelfare.client.id}")
+//    private static String YouthClientId;
+//
+//    @Value("${centerwelfare.client.secret}")
+//    private static String YouthSecret;
+//
+//    @Value("${centerwelfare.url.}")
+//    private static String YouthWelfareUrl;
 
 }
