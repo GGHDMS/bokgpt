f44e8f2159b4dd1d091d76598060df506f3b880a
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-17 16:58:15 +0900

    feat: 로그인 및 회원가입 시 dto 에 예외 처리 추가

--- src/main/java/kr/ac/bokgpt/security/dto/LoginDto.java
+++ src/main/java/kr/ac/bokgpt/security/dto/LoginDto.java
@@ -1,10 +1,13 @@
 package kr.ac.bokgpt.security.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record LoginDto(
+        @NotBlank(message = "email은 blank 일 수 없습니다!")
         String email,
+        @NotBlank(message = "password는 blank 일 수 없습니다!")
         String password
 ) {
 }
