2742e3df63a50adc9803b43da629d5ae527101eb
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-17 10:49:48 +0900

    feat: 인증 정보를 쉽게 가져오기 위한 SecurityUtil 구현

---
+++ src/main/java/kr/ac/bokgpt/security/util/SecurityUtil.java
@@ -0,0 +1,32 @@
+package kr.ac.bokgpt.security.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+@Slf4j
+public class SecurityUtil {
+
+   private SecurityUtil() {}
+
+   public static Optional<String> getCurrentEmail() {
+      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+      if (authentication == null) {
+         log.debug("Security Context에 인증 정보가 없습니다.");
+         return Optional.empty();
+      }
+
+      String email = null;
+      if (authentication.getPrincipal() instanceof UserDetails springSecurityUser) {
+         email = springSecurityUser.getUsername();
+      } else if (authentication.getPrincipal() instanceof String) {
+         email = (String) authentication.getPrincipal();
+      }
+
+      return Optional.ofNullable(email);
+   }
+}
