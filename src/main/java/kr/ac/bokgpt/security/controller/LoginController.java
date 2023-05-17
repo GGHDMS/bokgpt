8e0c3cbdf1d21c15d18e332945587a7121abbdf7
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-17 19:01:45 +0900

    refactor: 코드 수정
    불 필요 부분 삭제와 login 에서도 회원 가입시 사용하는 dto 를 사용 하고 있어서 수정

--- src/main/java/kr/ac/bokgpt/security/controller/LoginController.java
+++ src/main/java/kr/ac/bokgpt/security/controller/LoginController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.ac.bokgpt.jwt.JwtFilter;
 import kr.ac.bokgpt.jwt.TokenProvider;
-import kr.ac.bokgpt.security.dto.signupDto;
+import kr.ac.bokgpt.security.dto.LoginDto;
 import kr.ac.bokgpt.security.dto.TokenDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,7 +18,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
@@ -39,10 +42,10 @@ public class LoginController {
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
     })
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody signupDto signupDto) {
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
 
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(signupDto.email(), signupDto.password());
+                new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.password());
 
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
