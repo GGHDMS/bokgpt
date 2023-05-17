3d6fa7d9150467d47b4d25db9499584b9c6fd23c
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-17 16:57:43 +0900

    feat: 레포지토리 생성

---
+++ src/main/java/kr/ac/bokgpt/repository/relationship/member/MemberHomeTypeRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.bokgpt.repository.relationship.member;
+
+import kr.ac.bokgpt.domain.relationship.member.MemberHomeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberHomeTypeRepository extends JpaRepository<MemberHomeType, Long> {
+}
