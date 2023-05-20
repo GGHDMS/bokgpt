6dca5245b65d10b1415bd11262d814625f29494e
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-20 22:27:33 +0900

    feat: config 설정 변경
    Security 사용으로 JpaAuditor 사용에서 유저 email 을  가져올수 있게 하였다
    인증이 필요한 url 이 추가됨

--- src/main/java/kr/ac/bokgpt/config/JpaConfig.java
+++ src/main/java/kr/ac/bokgpt/config/JpaConfig.java
@@ -1,17 +1,18 @@
 package kr.ac.bokgpt.config;
 
+import kr.ac.bokgpt.security.util.SecurityUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
+@Slf4j
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.ofNullable("bokgpt");
+        return SecurityUtil::getCurrentEmail;
     }
 }
