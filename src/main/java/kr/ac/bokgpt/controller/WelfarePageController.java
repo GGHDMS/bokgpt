ff3ab3e988f4f35c36884499a36542fdd1720898
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-21 14:14:13 +0900

    feat: 복지 서비스 조회 컨트롤러 구현
    request dto 사용 말고 requestParam 으로 직접 값을 받는다
    query string validation 을 검사하였다

--- src/main/java/kr/ac/bokgpt/controller/WelfarePageController.java
+++ src/main/java/kr/ac/bokgpt/controller/WelfarePageController.java
@@ -4,16 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.ac.bokgpt.domain.Gender;
 import kr.ac.bokgpt.dto.WelfareAllInfoDto;
 import kr.ac.bokgpt.dto.WelfareTitleWithLifeCyclesDto;
 import kr.ac.bokgpt.service.WelfareService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/welfares")
@@ -22,7 +21,31 @@ public class WelfarePageController {
 
     public final WelfareService welfareService;
 
-    @Tag(name="business")
+    @Tag(name = "business")
+    @Operation(summary = "get welfares", description = "query string 에 따라 검색")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @GetMapping
+    public ResponseEntity<Page<WelfareTitleWithLifeCyclesDto>> searchWelfares(
+            @RequestParam(required = false) Gender gender,
+            @RequestParam(required = false, defaultValue = "0") Long lifeCycleId,
+            @RequestParam(required = false, defaultValue = "0") Long locationId,
+            @RequestParam(required = false, defaultValue = "0") Long homeTypeId,
+            @RequestParam(required = false, defaultValue = "0") Long interestThemeId,
+            Pageable pageable
+    ) {
+        if (!validateParameters(lifeCycleId, locationId, homeTypeId, interestThemeId)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(welfareService.searchWelfares(gender, lifeCycleId, locationId, homeTypeId, interestThemeId, pageable));
+    }
+
+    @Tag(name = "business")
     @Operation(summary = "get welfare_by_id", description = "간략한 정보의 복지정보 여러개를 가져오는 api입니다. (구현중)")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK !!"),
@@ -35,20 +58,7 @@ public class WelfarePageController {
         return welfareService.searchWelfareDetail(welfareId);
     }
 
-    @Tag(name="business")
-    @Operation(summary = "get welfares", description = "간략한 정보의 복지정보 여러개를 가져오는 api입니다. (구현중)")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK !!"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
-            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
-    })
-    @GetMapping
-    public  Page<WelfareTitleWithLifeCyclesDto> searchWelfares(Pageable pageable) {
-        return welfareService.searchWelfarePages(pageable);
-    }
-
-    @Tag(name="business")
+    @Tag(name = "business")
     @Operation(summary = "get interest-themes by interestThemeId", description = "간략한 정보의 복지정보 여러개를 가져오는 api입니다. (구현중)")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK !!"),
@@ -59,5 +69,22 @@ public class WelfarePageController {
     @GetMapping("/interest-themes/{interestThemeId}")
     public Page<WelfareTitleWithLifeCyclesDto> searchWelfareByInterestTheme(@PathVariable Long interestThemeId, Pageable pageable) {
         return welfareService.searchWelfareByInterestTheme(interestThemeId, pageable);
+    }
+
+    private boolean validateParameters(Long lifeCycleId, Long locationId, Long homeTypeId, Long interestThemeId) {
+        if (lifeCycleId < 0 || lifeCycleId > 7) {
+            return false;
+        }
+        if (locationId < 0 || locationId > 27) {
+            return false;
+        }
+        if (homeTypeId < 0 || homeTypeId > 6) {
+            return false;
+        }
+        if (interestThemeId < 0 || interestThemeId > 15) {
+            return false;
+        }
+
+        return true;
     }
 }
